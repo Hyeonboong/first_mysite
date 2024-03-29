c3c8595b0c4fa9939877b6eaa663935cbc4b65e7
ParkHyeonsu <krystal6345@naver.com>
2024-03-29 17:05:11 +0900

    result

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
-    path("menu", views.menu, name="etc"),
-    path("order", views.order_coffee, name="coffee")
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
